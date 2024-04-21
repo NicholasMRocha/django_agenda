95c33e48e9091bd434c13a620d3d4a612765c9a2
Nicholas Marques <nickmarquesdarocha@hotmail.com>
2024-04-21 17:52:02 -0300

    ARQUIVO STYLE.CSS completo

--- contact/migrations/0003_category_contact_category.py
+++ contact/migrations/0003_category_contact_category.py
@@ -7,7 +7,7 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('contact', '0002_contact_picture_contact_show'),
+        ('contact', '0002_contact_picture_contact_show_alter_contact_last_name'),
     ]
 
     operations = [

a73369b45ab138856f26b091872afce83724228a
Nicholas Marques <nickmarquesdarocha@hotmail.com>
2024-05-01 16:59:52 -0300

    Adicionei CRUD dos contatos

--- contact/urls.py
+++ contact/urls.py
@@ -5,7 +5,12 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('<int:contact_id>/', views.contact, name='contact'),  # type:ignore
-    path('search/', views.search, name='search'),  # type:ignore
     path('', views.index, name='index'),  # type:ignore
+    path('search/', views.search, name='search'),  # type:ignore
+
+    # contact (CRUD)
+    path('contact/create/', views.contact, name='contact'),  # type:ignore
+    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),  # type:ignore
+    path('contact/<int:contact_id>/update/', views.contact, name='contact'),  # type:ignore
+    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),  # type:ignore
 ]

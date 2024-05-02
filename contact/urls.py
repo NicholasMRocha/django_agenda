1eea1fbe3e2654b2ff60a043c2ee5bfbd88c138c
Nicholas Marques <nickmarquesdarocha@hotmail.com>
2024-05-02 09:29:02 -0300

    Começando as atualizações para montar o CRUD dos contatos

--- contact/urls.py
+++ contact/urls.py
@@ -1,3 +1,4 @@
+# type:ignore
 from django.urls import path
 
 from contact import views
@@ -5,12 +6,10 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
-    path('', views.index, name='index'),  # type:ignore
-    path('search/', views.search, name='search'),  # type:ignore
+    path('', views.index, name='index'),
+    path('search/', views.search, name='search'),
 
     # contact (CRUD)
-    path('contact/create/', views.contact, name='contact'),  # type:ignore
-    path('contact/<int:contact_id>/detail/', views.contact, name='contact'),  # type:ignore
-    path('contact/<int:contact_id>/update/', views.contact, name='contact'),  # type:ignore
-    path('contact/<int:contact_id>/delete/', views.contact, name='contact'),  # type:ignore
+    path('contact/<int:contact_id>/', views.contact, name='contact'),
+    path('contact/create/', views.create, name='create'),
 ]

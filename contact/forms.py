a8b150ac8241d158b8f1266fd5da4e8ffc38b820
Nicholas Marques <nickmarquesdarocha@hotmail.com>
2024-05-19 19:40:43 -0300

    Create a form to register a new user

--- contact/forms.py
+++ contact/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from django.core.exceptions import ValidationError
 
 from . import models
@@ -13,14 +14,6 @@ class ContactForm(forms.ModelForm):
         )
     )
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
-        # self.fields['first_name'].widget.attrs.update({
-        #     'class': 'classe-a classe-b',
-        #     'placeholder': 'Aqui veio do init',
-        # })
-
     class Meta:
         model = models.Contact
         fields = (
@@ -28,33 +21,22 @@ class ContactForm(forms.ModelForm):
             'email', 'description', 'category',
             'picture',
         )
-        # widgets = {
-        #     'first_name': forms.TextInput(
-        #         attrs={
-        #             'class': 'classe-a blasse-b',
-        #             'placeholder': 'Escreva aqui',
-        #         }
-        #     )
-        # }
 
     def clean(self):
         cleaned_data = self.cleaned_data
         first_name = cleaned_data.get('first_name')
         last_name = cleaned_data.get('last_name')
-
         if first_name == last_name:
             msg = ValidationError(
-                    'Primeiro nome não pode ser igual ao segundo',
-                    code='invalid',
-                )
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
             self.add_error('first_name', msg)
             self.add_error('last_name', msg)
-
         return super().clean()
 
     def clean_first_name(self):
         first_name = self.cleaned_data.get('first_name')
-
         if first_name == 'ABC':
             self.add_error(
                 'first_name',
@@ -65,3 +47,7 @@ class ContactForm(forms.ModelForm):
             )
 
         return first_name
+
+
+class RegisterForm(UserCreationForm):
+    ...
